dd518625ad49bdeb30057f622150ed631625cf58
Valeria Silveira <ssilveira@nosis.com>
2023-11-13 17:05:48 -0300

    config ckeditor

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'blog',
-    'users'
+    'users',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/users/login'
