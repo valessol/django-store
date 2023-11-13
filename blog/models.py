a5318c53d3df3b36bcfcb8a4f063d970b4ae39b0
Valeria Silveira <ssilveira@nosis.com>
2023-11-13 17:01:05 -0300

    update entry model

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+from datetime import date
 
 class BlogEntry(models.Model):
-    title = models.CharField(max_length=50)
-    description = RichTextField()
-    image = models.ImageField()
-    outstanding = models.BooleanField(default=False)
+    title = models.CharField(max_length=50, verbose_name='Título')
+    description = RichTextField(verbose_name='Descripción')
+    image = models.ImageField(verbose_name='Imagen')
+    outstanding = models.BooleanField(default=False, verbose_name='Destacar')
     review = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, verbose_name='Categoría')
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateField()
+    created_at = models.DateField(default=date.today)
 
 class Comment(models.Model):
     comment = models.TextField()
     entry_id = models.ForeignKey(BlogEntry, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateField()
+    created_at = models.DateField(default=date.today)
