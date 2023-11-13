e53d4835ee297b7c25013774b49be6c45b7b512a
Valeria Silveira <ssilveira@nosis.com>
2023-11-13 17:02:38 -0300

    add blog views, url & templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from blog.views import EntriesList, CreateEntry
+from blog.views import EntriesList, CreateEntry, EditEntry, EntryView, DeleteEntry
 
 urlpatterns = [
     path('', EntriesList.as_view(), name='entries'),
-    path('entry/create', CreateEntry.as_view(), name='create_entry')
+    path('entry/create', CreateEntry.as_view(), name='create_entry'),
+    path('entry/<int:pk>/', EntryView.as_view(), name='entry'),
+    path('entry/<int:pk>/edit', EditEntry.as_view(), name='edit_entry'),
+    path('entry/<int:pk>/delete', DeleteEntry.as_view(), name='delete_entry'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
-from blog.models import BlogEntry
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from blog.models import BlogEntry
+
+# Class-based views
 
 class EntriesList(ListView):
     model = BlogEntry
     context_object_name = 'entries_list'
     template_name = 'blog/index.html'
+    
+    def get_queryset(self):
+        search = self.request.GET.get('search', '')
+        if search:
+            entries_list = self.model.objects.filter(title__icontains=title)
+        else:
+            entries_list = self.model.objects.all()
+        return entries_list
 
-class CreateEntry(CreateView):
+class CreateEntry(LoginRequiredMixin, CreateView):
     model = BlogEntry
     template_name = 'blog/create_entry.html'
-    fields = ['title', 'description', 'image', 'outstanding', 'category']
+    fields = ['title', 'description', 'image', 'category']
+    success_url = reverse_lazy('')
+    
+class EditEntry(LoginRequiredMixin, UpdateView):
+    model = BlogEntry
+    template_name = 'blog/edit_entry.html'
+    fields = ['title', 'description', 'image', 'category']
+    success_url = reverse_lazy('entry/<int:pk>/')
+    
+class EntryView(DetailView):
+    model = BlogEntry
+    context_object_name = 'entry'
+    template_name = 'blog/entry_view.html'
+    
+class DeleteEntry(LoginRequiredMixin, DeleteView):
+    model = BlogEntry
+    template_name = 'blog/delete_entry.html'
     success_url = reverse_lazy('')
